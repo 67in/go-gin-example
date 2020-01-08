803c89700e6a946ae88a9d4f0731531e625c1846
i-liuqin <i-liuqin@360.com>
2020-01-08 20:46:31 +0800

    blog 2.1

--- main.go
+++ main.go
@@ -2,33 +2,28 @@ package main
 
 import (
 	"fmt"
-	 "github.com/67in/go-gin-example/pkg/setting"
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/67in/go-gin-example/routers"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
-
-	s := &http.Server{
-		Addr:              fmt.Sprintf(":%d", setting2.HTTPPort),
-		Handler:           router,
-		TLSConfig:         nil,
-		ReadTimeout:       setting.ReadTimeout,
-		ReadHeaderTimeout: 0,
-		WriteTimeout:      setting.WriteTimeout,
-		IdleTimeout:       0,
-		MaxHeaderBytes:    1 << 20,
-		TLSNextProto:      nil,
-		ConnState:         nil,
-		ErrorLog:          nil,
-		BaseContext:       nil,
-		ConnContext:       nil,
-	}
-	s.ListenAndServe()
+	r := routers.InitRouter()
+	_ = r.Run(fmt.Sprintf(":#{Port}"))
+	//router.GET("")
+	//
+	//s := &http.Server{
+	//	Addr:              fmt.Sprintf(":%d", setting.HTTPPort),
+	//	Handler:           router,
+	//	TLSConfig:         nil,
+	//	ReadTimeout:       setting.ReadTimeout,
+	//	ReadHeaderTimeout: 0,
+	//	WriteTimeout:      setting.WriteTimeout,
+	//	IdleTimeout:       0,
+	//	MaxHeaderBytes:    1 << 20,
+	//	TLSNextProto:      nil,
+	//	ConnState:         nil,
+	//	ErrorLog:          nil,
+	//	BaseContext:       nil,
+	//	ConnContext:       nil,
+	//}
+	//s.ListenAndServe()
 }
