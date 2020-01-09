fc154df575aeaf3452ce08d534110ee0594b82e9
i-liuqin <i-liuqin@360.com>
2020-01-09 17:06:05 +0800

    blog 2

--- main.go
+++ main.go
@@ -2,28 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/67in/go-gin-example/pkg/setting"
 	"github.com/67in/go-gin-example/routers"
+	"net/http"
 )
 
 func main() {
 	r := routers.InitRouter()
-	_ = r.Run(fmt.Sprintf(":#{Port}"))
+//	_ = r.Run()
 	//router.GET("")
-	//
-	//s := &http.Server{
-	//	Addr:              fmt.Sprintf(":%d", setting.HTTPPort),
-	//	Handler:           router,
-	//	TLSConfig:         nil,
-	//	ReadTimeout:       setting.ReadTimeout,
-	//	ReadHeaderTimeout: 0,
-	//	WriteTimeout:      setting.WriteTimeout,
-	//	IdleTimeout:       0,
-	//	MaxHeaderBytes:    1 << 20,
-	//	TLSNextProto:      nil,
-	//	ConnState:         nil,
-	//	ErrorLog:          nil,
-	//	BaseContext:       nil,
-	//	ConnContext:       nil,
-	//}
-	//s.ListenAndServe()
+	s := &http.Server{
+		Addr:              fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:           r,
+		TLSConfig:         nil,
+		ReadTimeout:       setting.ReadTimeout,
+		ReadHeaderTimeout: 0,
+		WriteTimeout:      setting.WriteTimeout,
+		IdleTimeout:       0,
+		MaxHeaderBytes:    1 << 20,
+		TLSNextProto:      nil,
+		ConnState:         nil,
+		ErrorLog:          nil,
+		BaseContext:       nil,
+		ConnContext:       nil,
+	}
+	s.ListenAndServe()
 }
